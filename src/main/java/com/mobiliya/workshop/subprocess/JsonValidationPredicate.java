1eca9518b3f155f34df591d3c768469982ec5752
Abhishek Vaze <abhishek.vaze@mobiliya.com>
2019-11-21 16:21:54 +0530

    working

--- src/main/java/com/mobiliya/workshop/subprocess/JsonValidationPredicate.java
+++ src/main/java/com/mobiliya/workshop/subprocess/JsonValidationPredicate.java
@@ -7,6 +7,7 @@ import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.Serializable;
 import java.util.function.Predicate;
 
@@ -15,11 +16,15 @@ public class JsonValidationPredicate implements Predicate<String>, Serializable 
     public boolean test(String input) {
         try {
             JsonNode inputJson = JsonLoader.fromString(input);
-            JsonNode inputSchema = JsonLoader.fromFile(new File("C:\\Users\\Abhishek\\IdeaProjects\\pipelineexample\\src\\main\\resources\\input-message-schema.json"));
+            JsonNode inputSchema = JsonLoader.fromFile(new File("src\\main\\resources\\input-message-schema.json"));
             JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
             JsonSchema schema = factory.getJsonSchema(inputSchema);
             ProcessingReport report = schema.validate(inputJson);
             return report.isSuccess();
+        } catch (FileNotFoundException fex) {
+            System.err.println(fex.getMessage());
+            System.exit(-1);
+            return false;
         } catch (Exception ex) {
             return false;
         }
